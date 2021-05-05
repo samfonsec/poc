1ddf604696b2620a2b26b1bea2a0430ccf6c1184
Warlen <magnos22@gmail.com>
2021-05-05 12:39:03 -0300

    initial implementation navigator

--- navigator/src/main/java/com/example/navigator/core/Navigator.kt
+++ navigator/src/main/java/com/example/navigator/core/Navigator.kt
@@ -1,6 +1,6 @@
 package com.example.navigator.core
 
-open class Navigator() {
+open class Navigator {
 
     fun loadIntent(packageName: String, module: String) = ClassRegistry.loadIntent(packageName, module)
 }
