e856222c2ee9644a80389e55e55ade6a3e0a4def
Warlen <magnos22@gmail.com>
2021-05-11 17:50:03 -0300

    implementation lib Quadrant

---
+++ app/src/main/java/com/samfonsec/poczinha/navigate/Navigator.kt
@@ -0,0 +1,11 @@
+package com.samfonsec.poczinha.navigate
+
+import android.content.Context
+import android.content.Intent
+
+interface NavigatorProvider {
+    fun navigate(context: Context): Intent
+}
+
+fun NavigatorProvider.getIntentForClassName(context: Context, className: String) =
+    Intent().setClassName(context, className)
